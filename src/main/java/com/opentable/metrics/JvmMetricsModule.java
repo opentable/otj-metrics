35cb4b617c5d0a8f17a6327d3943489ee5fe95f2
Steven Schlansker <sschlansker@opentable.com>
2015-04-17 15:33:01 -0700

    Prepend 'jvm-' to all jvm statistics to disambiguate

--- src/main/java/com/opentable/metrics/JvmMetricsModule.java
+++ src/main/java/com/opentable/metrics/JvmMetricsModule.java
@@ -1,13 +1,18 @@
 package com.opentable.metrics;
 
+import java.util.Map;
+
 import javax.management.MBeanServer;
 
+import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.MetricSet;
 import com.codahale.metrics.jvm.BufferPoolMetricSet;
 import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
 import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
 import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 
@@ -24,12 +29,25 @@ public final class JvmMetricsModule extends AbstractModule
         @Inject
         JvmMetricsSets(MetricRegistry metrics, MBeanServer mbs)
         {
-            metrics.registerAll(new BufferPoolMetricSet(mbs));
-            metrics.register("fd-used-ratio", new FileDescriptorRatioGauge());
-            metrics.registerAll(new GarbageCollectorMetricSet());
-            metrics.registerAll(new MemoryUsageGaugeSet());
-            metrics.registerAll(new ThreadStatesGaugeSet());
+            metrics.registerAll(prependJvm(new BufferPoolMetricSet(mbs)));
+            metrics.register("jvm-fd-used-ratio", new FileDescriptorRatioGauge());
+            metrics.registerAll(prependJvm(new GarbageCollectorMetricSet()));
+            metrics.registerAll(prependJvm(new MemoryUsageGaugeSet()));
+            metrics.registerAll(prependJvm(new ThreadStatesGaugeSet()));
         }
+    }
+
+    private static MetricSet prependJvm(MetricSet metrics) {
+        ImmutableMap.Builder<String, Metric> builder = ImmutableMap.builder();
+        metrics.getMetrics().forEach((name, metric) -> builder.put("jvm-" + name, metric));
+        final ImmutableMap<String, Metric> built = builder.build();
+
+        return new MetricSet() {
+            @Override
+            public Map<String, Metric> getMetrics() {
+                return built;
+            }
+        };
     }
 
     @Override
