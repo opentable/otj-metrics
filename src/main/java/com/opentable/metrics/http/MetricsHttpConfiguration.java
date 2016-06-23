f55c93e2ac2678221a87e7021c63118a6c516661
Chris Pennello <cpennello@opentable.com>
2016-06-22 18:07:07 -0700

    Fixes Dropwizard Metrics AdminServlet setup.
    Trying to add the servlet manually doesn't work since it occurs too late
    in the process.  Instead, you're supposed to declare a
    [`ServletRegistrationBean`][1].
    
    [1]: http://docs.spring.io/spring-boot/docs/current/reference/html/howto-embedded-servlet-containers.html#howto-add-a-servlet-filter-or-listener-as-spring-bean

--- src/main/java/com/opentable/metrics/http/MetricsHttpConfiguration.java
+++ src/main/java/com/opentable/metrics/http/MetricsHttpConfiguration.java
@@ -1,8 +1,5 @@
 package com.opentable.metrics.http;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Named;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextListener;
 
 import com.codahale.metrics.MetricRegistry;
@@ -10,37 +7,25 @@ import com.codahale.metrics.servlets.AdminServlet;
 import com.codahale.metrics.servlets.MetricsServlet;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({
-        MetricsHttpResource.class,
-        MetricsHttpConfiguration.Servlet.class,
-})
+@Import(MetricsHttpResource.class)
 public class MetricsHttpConfiguration {
+    @Value("${ot.metrics.http.path:/metrics}")
+    private String path;
+
     @Bean
     public ServletContextListener getMetricsContextListener(final MetricRegistry metrics) {
         return new MetricsContextListener(metrics);
     }
 
-    @Named
-    static class Servlet {
-        @Value("${ot.metrics.http.path:/metrics}")
-        private String path;
-
-        private final ServletContext container;
-
-        Servlet(final ServletContext container) {
-            this.container = container;
-        }
-
-        @PostConstruct
-        public void postConstruct() {
-            final String urlPattern = path + "*";
-            container.addServlet(AdminServlet.class.getCanonicalName(), AdminServlet.class).addMapping(urlPattern);
-        }
+    @Bean
+    public ServletRegistrationBean getMetricsServlet() {
+        return new ServletRegistrationBean(new AdminServlet(), path + "/*");
     }
 
     private static class MetricsContextListener extends MetricsServlet.ContextListener {
