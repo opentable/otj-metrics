6d56613411241ab2d1b8c6fe0d649e1b0fb9d3d7
Karl Cempron <kcempron@opentable.com>
2017-07-12 14:46:37 -0700

    Modified number of counters and retrieved os method on initialization

--- src/main/java/com/opentable/metrics/jvm/FileDescriptorMetricSet.java
+++ src/main/java/com/opentable/metrics/jvm/FileDescriptorMetricSet.java
@@ -17,6 +17,9 @@ import com.codahale.metrics.MetricSet;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * A set of gauges for the count of open and maximum file descriptors.
@@ -24,31 +27,35 @@ import com.google.common.collect.ImmutableMap;
  * file descriptors from the OS.
  */
 public class FileDescriptorMetricSet implements MetricSet {
+    private static final Logger LOG = LoggerFactory.getLogger(FileDescriptorMetricSet.class);
     private final List<String> FDTYPE = ImmutableList.of("open", "max");
-    private final List<String> EXCEPTION = ImmutableList.of("no-such-method", "illegal-access", "invocation-target");
+    private final List<String> EXCEPTION = ImmutableList.of("illegal-access", "invocation-target");
     private final OperatingSystemMXBean os = ManagementFactory.getOperatingSystemMXBean();
     private final Map<String, Map<String, Counter>> calls;
+    private final Method openMethod;
+    private final Method maxMethod;
 
     public FileDescriptorMetricSet() {
         final Supplier<Map<String, Counter>> makeCounters = () ->
                 ImmutableMap.of(
-                        "no-such-method", new Counter(),
                         "illegal-access", new Counter(),
                         "invocation-target", new Counter()
                 );
-
         calls = ImmutableMap.of(
                 "open", makeCounters.get(),
                 "max", makeCounters.get()
         );
+
+        openMethod = retrieveMethod("getOpenFileDescriptorCount");
+        maxMethod = retrieveMethod("getMaxFileDescriptorCount");
     }
 
     @Override
     public Map<String, Metric> getMetrics() {
         final Map<String, Metric> gauges = new HashMap<>();
 
-        gauges.put(FDTYPE.get(0), (Gauge<Long>)() -> invoke("getOpenFileDescriptorCount"));
-        gauges.put(FDTYPE.get(1), (Gauge<Long>)() -> invoke("getMaxFileDescriptorCount"));
+        gauges.put(FDTYPE.get(0), (Gauge<Long>)() -> invoke(openMethod, FDTYPE.get(0)));
+        gauges.put(FDTYPE.get(1), (Gauge<Long>)() -> invoke(maxMethod, FDTYPE.get(1)));
 
         for (String type: FDTYPE) {
             for(String ex: EXCEPTION) {
@@ -58,18 +65,24 @@ public class FileDescriptorMetricSet implements MetricSet {
         return Collections.unmodifiableMap(gauges);
     }
 
-    private Long invoke(String name) {
-        String fdType =  (name.contains("open")) ? FDTYPE.get(0) : FDTYPE.get(1);
+    private Method retrieveMethod(String name) {
         try {
             final Method method = os.getClass().getDeclaredMethod(name);
             method.setAccessible(true);
-            return (Long) method.invoke(os);
+            return method;
         } catch (NoSuchMethodException e) {
-            calls.get(fdType).get(EXCEPTION.get(0)).inc();
+            LOG.error("No such method exists:", e);
+            return null;
+        }
+    }
+
+    private Long invoke(Method method, String name) {
+        try {
+            return (Long) method.invoke(os);
         } catch (IllegalAccessException e) {
-            calls.get(fdType).get(EXCEPTION.get(1)).inc();
+            calls.get(name).get(EXCEPTION.get(0)).inc();
         } catch (InvocationTargetException e) {
-            calls.get(fdType).get(EXCEPTION.get(2)).inc();
+            calls.get(name).get(EXCEPTION.get(1)).inc();
         }
         return null;
     }
