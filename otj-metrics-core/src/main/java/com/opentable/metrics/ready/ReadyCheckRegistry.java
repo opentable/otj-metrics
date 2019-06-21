fb9ead2df14ec160b668f31e78b30195f303bd72
Michael Bell <mbell@opentable.com>
2019-06-21 15:12:06 -0700

    fix

--- otj-metrics-core/src/main/java/com/opentable/metrics/ready/ReadyCheckRegistry.java
+++ otj-metrics-core/src/main/java/com/opentable/metrics/ready/ReadyCheckRegistry.java
@@ -10,7 +10,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package com.opentable.metrics.ready;
+ */
+package com.opentable.metrics.ready;
 
 import java.time.Duration;
 import java.util.Collections;
