a8473841c2f4dda2ff7cfe842f9203c07941a8e9
Janardhan Singh <jsingh@opentable.com>
2021-12-03 09:39:39 +0530

    Formatting fix

--- otj-metrics-micrometer/src/main/java/com/opentable/metrics/micrometer/MeterFilterUtils.java
+++ otj-metrics-micrometer/src/main/java/com/opentable/metrics/micrometer/MeterFilterUtils.java
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.opentable.metrics.micrometer;
 
 import static java.util.stream.Collectors.toList;
